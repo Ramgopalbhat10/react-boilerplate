d1a4d339c564d8394e75444a20c95ec841806ecd
Jarvis <subramanyaramgopal@gmail.com>
2018-12-18 22:20:24 +0530

    Added polyfill and React lazy loading

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,8 +14,32 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: [
+                  "last 2 versions",
+                  "not dead",
+                  "not < 2%",
+                  "not ie 11"
+                ],
+                useBuiltIns: "entry"
+              }
+            ],
+            "@babel/preset-react"
+          ],
+          plugins: [
+            "react-hot-loader/babel",
+            "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-syntax-dynamic-import"
+          ]
         }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /node_modeules/
       }
     ]
   },
